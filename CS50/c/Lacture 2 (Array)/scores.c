d4447b2f7fabaa24a2dec214f16e69e68d7efa0a
Asraf1270 <mdasrafulislam1270@gmail.com>
2026-04-21 17:23:41 +0600

    Refactor scores.c to use an array for score calculations and clean up commented code

--- CS50/c/Lacture 2 (Array)/scores.c
+++ CS50/c/Lacture 2 (Array)/scores.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(viod)
 {
+    /*
     int score1 = 90;
     int score2 = 80;
     int score3 = 70;
@@ -8,5 +9,15 @@ int main(viod)
     //Print Agerage
     printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
     // If we divide by 3, it will be an integer division and the result will be an integer( or show error). 
-    // To get a floating-point result, we need to divide by 3.0 (a floating-point number).
+    // To get a floating-point result, we need to divide by 3.0 (a floating-point number).*/
+
+
+    // Using array
+    int scores[3];
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
+
+    //Print Average
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
