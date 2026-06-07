b8b73c47eebe8552c824d168685446c8c0b9ee25
Asraf1270 <mdasrafulislam1270@gmail.com>
2026-06-07 14:57:08 +0600

    Add Flask application with greeting functionality and HTML templates
    - Implement main Flask app with routes for home and greet
    - Create HTML templates: index.html, index2.html, and greet.html
    - Add .gitignore entries for Lecture 5 and virtual environment
    - Create LECTURE.md and requrement.txt files

---
+++ CS50/Flask/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    if "name" in request.args:
+        name = request.args["name"]
+    else:
+        name = "world"
+    return render_template("index.html", name = name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
